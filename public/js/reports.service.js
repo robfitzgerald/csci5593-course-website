e8e2af5addddb088cc12c94296b32d71d4e4570f
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-01-26 13:19:39 -0700

    fixed date

--- public/js/reports.service.js
+++ public/js/reports.service.js
@@ -16,7 +16,7 @@
 					articleTitle: 'Lit Review: Reimagining Heterogeneous Computing: a Functional Instruction Set Architecture (F-ISA) Computing Model',
 					articleAuthors: 'Daniel Nemirovsky, Nikola Markovic, Osman Unsal, Mateo Valero, Adrian Cristal',
 					articleSource: 'IEEE Micro',
-					reviewDate: new Date(2015, 11, 11),
+					reviewDate: new Date(2017, 0, 26),
 					articleURL: 'http://ieeexplore.ieee.org/abstract/document/7325210/',
 					download: 'reviews/robfitzgerald-review-01.pdf',
 					description: 'Proposing a new ISA for Heterogeneous Computing architectures.'
