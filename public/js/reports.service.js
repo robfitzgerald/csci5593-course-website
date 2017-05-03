41a4f7c9134c32bd431efc115011582fe300a1ad
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-05-02 21:36:17 -0600

    added remaining course requirements.

--- public/js/reports.service.js
+++ public/js/reports.service.js
@@ -20,7 +20,43 @@
 					articleURL: 'http://ieeexplore.ieee.org/abstract/document/7325210/',
 					download: 'reviews/robfitzgerald-review-01.pdf',
 					description: 'Proposing a new ISA for Heterogeneous Computing architectures.'
-				}
+				},
+				{
+					articleTitle: 'Project Repository',
+					articleAuthors: '',
+					articleSource: '',
+					reviewDate: new Date(2017, 4, 2),
+					articleURL: 'https://github.com/robfitzgerald/csci5593-project',
+					download: 'reviews/csci5593-project-master.zip',
+					description: 'Open MPI Profiling App Repository'
+				},
+				{
+					articleTitle: 'Final Report',
+					articleAuthors: 'Nathan Justice, Matthew Kalan, Rob Fitzgerald',
+					articleSource: 'CSCI-5593',
+					reviewDate: new Date(2017, 4, 2),
+					articleURL: '',
+					download: 'reviews/MappingHeraclesCommunicationNetworkDataandAnalysis.docx',
+					description: 'Experimental Results'
+				},
+				{
+					articleTitle: 'Research Presentation',
+					articleAuthors: 'Nathan Justice, Matthew Kalan, Rob Fitzgerald',
+					articleSource: 'CSCI-5593',
+					reviewDate: new Date(2017, 3, 27),
+					articleURL: '',
+					download: 'reviews/Team_8_presentation-Mapping_Heracles_Communication-.pptx',
+					description: 'Presentation of project concept and tentative implementation plan'
+				},
+				{
+					articleTitle: 'Project Presentation',
+					articleAuthors: 'Nathan Justice, Matthew Kalan, Rob Fitzgerald',
+					articleSource: 'CSCI-5593',
+					reviewDate: new Date(2017, 4, 2),
+					articleURL: '',
+					download: 'reviews/Team_8-Mapping_Heracles_Implementation.pptx',
+					description: 'Demonstration of initial experimental results'
+				}					
 			]
 		}
 	}
